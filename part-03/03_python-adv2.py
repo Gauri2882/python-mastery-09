54689812e6d8a76000f61de22face2286a085a6d
Gauri <gauripatil2999@gmail.com>
2025-08-02 20:47:49 +0530

    Project 62: Movie Recommendation System

---
+++ part-03/03_python-adv2.py
@@ -0,0 +1,39 @@
+""" Project: Movie Recommendation System """
+
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+
+def load_dataset(file_path):
+    return pd.read_csv(file_path)
+
+def calculate_similarity(matrix):
+    similarity = cosine_similarity(matrix)
+    return pd.DataFrame(similarity, index= matrix.index, columns= matrix.index)
+
+def recommend_movies(user_id, ratings_matrix, user_similarity):
+    similar_users = user_similarity[user_id].sort_values(ascending=False).index[1:]
+    recommended_movies = {}
+
+    for similar_user in similar_users:
+        watched_movies = ratings_matrix.loc[similar_user][ratings_matrix.loc[similar_user] > 0]
+        for movie, rating in watched_movies.items():
+            if ratings_matrix.loc[user_id, movie] == 0:
+                if movie not in recommended_movies:
+                    recommended_movies[movie] = 0
+                recommended_movies[movie] += rating
+    return sorted(recommended_movies.items(), key=lambda x: x[1], reverse=True)
+
+def main():
+    print("Welcome to Movie Recommendation System")
+    ratings = load_dataset("part-03/movie_ratings.csv")
+    ratings_matrix = ratings.pivot_table(index = "user", columns= "movie", values = "rating").fillna(0)
+    user_similarity = calculate_similarity(ratings_matrix)
+
+    user_id = int(input("Enter the user ID for recommendation: "))
+    recommendations = recommend_movies(user_id, ratings_matrix, user_similarity)
+    print(f"\nRecommendation for user {user_id}: ")
+    for movie, score in recommendations:
+        print(f"{movie}: {score}")
+
+if __name__ == "__main__":
+    main()
